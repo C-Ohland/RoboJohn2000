58f9d557709a9c42df8f8f2816b776d1f2f60fe8
Carson Ohland <chohland@gmail.com>
2023-01-26 22:05:47 -0600

    removed token log

--- robojohn.js
+++ robojohn.js
@@ -9,7 +9,6 @@ const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 require('dotenv').config();
 var gameList;
 
-console.log(process.env.DISCORD_TOKEN);
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
